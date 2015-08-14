ade1551142650671c9e7b0c43f28c3df417f7882
Dom McKellar <dom.mc@outlook.com>
2015-08-14 10:43:43 -0700

    seperate sections and edits to scss

---
+++ resources/views/partials/_featured.blade.php
@@ -0,0 +1,29 @@
+<section id="featured" class="carousel fade" data-ride="carousel">
+  <ol class="carousel-indicators">
+  </ol>
+  <div class="carousel-inner fullheight">
+    <div class="item"><img src="img/story/2007b.jpg" alt=""></div>
+    <div class="item"><img src="img/story/2007c.jpg" alt=""></div>
+    <div class="item"><img src="img/story/2007a.jpg" alt=""></div>
+    <div class="item"><img src="img/story/2008.jpg" alt=""></div>
+    <div class="item"><img src="img/story/2008a.jpg" alt=""></div>
+    <div class="item"><img src="img/story/2008c.jpg" alt=""></div>
+    <div class="item"><img src="img/story/2008b.jpg" alt=""></div>
+    <div class="item"><img src="img/story/2009.jpg" alt=""></div>
+    <div class="item"><img src="img/story/2009a.jpg" alt=""></div>
+    <div class="item"><img src="img/story/2010c.jpg" alt=""></div>
+    <div class="item"><img src="img/story/2012.jpg" alt=""></div>
+    <div class="item"><img src="img/story/2013a.jpg" alt=""></div>
+    <div class="item"><img src="img/story/2014c.jpg" alt=""></div>
+    <div class="item"><img src="img/story/2014.jpg" alt=""></div>
+    <div class="item"><img src="img/story/2014a.jpg" alt=""></div>
+    <div class="item"><img src="img/story/2014b.jpg" alt=""></div>
+  </div>{{-- /.carousel-inner. .fullheight --}}
+
+  <a class="left carousel-control" href="#featured" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#featured" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</section>
--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -1,50 +1,27 @@
-  <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand hidden-sm hidden-md" href="#rsvp"><h1>Nakissa &amp; Dom</h1></a> {{-- hidden at 1200px and smaller screens --}}
-      </div> {{-- .navbar-header --}}
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand hidden-sm hidden-md" href="#rsvp"><h1>Nakissa &amp; Dom</h1></a>{{-- hidden at 1200px and smaller screens --}}
+    </div>{{-- /.navbar-header --}}
 
-      <div class="collapse navbar-collapse" id="collapse">
-        <ul class="nav navbar-nav navbar-right">
-          <li class="active"><a href="#rsvp">RSVP</a></li>
-          <li><a href="#bigday">Big Day</a></li>
-          <li><a href="#venue">Venue</a></li>
-          <li><a href="#room">Get a Room</a></li>
-          <li><a href="#fun">Island Fun</a></li>
-          <li><a href="#guestbook">Guestbook</a></li>
-          {{-- <li><a href="#featured">Our Story</a></li> --}}
-          <li><a href="#registry">Registry</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </div> {{-- #collapse .collapse .navbar-collapse --}}
-    </div> {{-- .container --}}
-  </nav>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <div id="collapse" class="collapse navbar-collapse" >
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="#rsvp">RSVP</a></li>
+        <li><a href="#bigday">Big Day</a></li>
+        <li><a href="#venue">Venue</a></li>
+        <li><a href="#room">Get a Room</a></li>
+        <li><a href="#fun">Island Fun</a></li>
+        <li><a href="#guestbook">Guestbook</a></li>
+        {{-- <li><a href="#featured">Our Story</a></li> --}}
+        <li><a href="#registry">Registry</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </div>{{-- /#collapse .collapse.navbar-collapse --}}
+  </div>{{-- /.container-fluid --}}
+</nav>
