897ff0adc7d895efad96aad5798d1b17bb72ed77
Dom McKellar <dom.mc@outlook.com>
2015-08-15 00:10:52 -0700

    9th ios nav test

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
   <div class="container-fluid">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapsex">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
@@ -10,7 +10,7 @@
       <a class="navbar-brand hidden-sm hidden-md" href="#rsvp"><h1>Nakissa &amp; Dom</h1></a>{{-- hidden at 1200px and smaller screens --}}
     </div>{{-- /.navbar-header --}}
 
-    <div id="collapse" class="collapse navbar-collapse" >
+    <div id="collapsex" class="collapse navbar-collapse" >
       <ul class="nav navbar-nav navbar-right">
         <li class="active"><a href="#rsvp">RSVP</a></li>
         <li><a href="#bigday">Big Day</a></li>
@@ -18,7 +18,7 @@
         <li><a href="#room">Get a Room</a></li>
         <li><a href="#fun">Island Fun</a></li>
         <li><a href="#guestbook">Guestbook</a></li>
-        {{-- <li><a href="#featured">Our Story</a></li> --}}
+  
         <li><a href="#registry">Registry</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
