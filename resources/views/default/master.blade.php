73b2b215dd41127e7b84a01b8f50c0ce37c592f3
Dom McKellar <dom.mc@outlook.com>
2015-08-15 00:27:08 -0700

    13th ios nav test

--- resources/views/default/master.blade.php
+++ resources/views/default/master.blade.php
@@ -14,7 +14,7 @@
 	{{-- My Custom stylesheet --}}
 
 
-	<link rel="stylesheet" type="text/css" href="css/all.css">
+	
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
