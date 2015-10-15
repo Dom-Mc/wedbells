82ec54e25c23810b3b368f00792c141466e0ae65
Dom <Dom-Mc@users.noreply.github.com>
2015-10-14 17:48:10 -1000

    Update _directions.blade.php

--- resources/views/partials/_directions.blade.php
+++ resources/views/partials/_directions.blade.php
@@ -6,19 +6,18 @@
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 
         <h2 class="modal-title text-center">Hale Wailele</h2>
-<h3>78-7102 Walua Road, Kailua-Kona, HI 96740</h3>
+<h3 class="text-center">78-7102 Walua Road, Kailua-Kona, HI 96740</h3>
 
       </div>{{-- /.modal-header --}}
 
       <div class="modal-body">
-<h3>Directions from Kona International Airport</h3>
+<h3 class="text-center">Directions from Kona International Airport</h3>
 <ul>
 <li>Drive south on Highway 19 which eventually becomes Highway 11</li>
 <li>Continue past Kamehameha III Rd (the street you'd turn on to get to the Sheraton</li>
 <li>Take a right on Ihilani Place</li>
 <li>Take a left on Walua Road (you'll see a sign and balloons)</li>
-<li>Continue through the front gate and down the hill until you've reached the venue.</li>
-<li><small>*Please do not block the driveway</small></li>
+<li>Continue through the front gate and down the hill until you've reached the venue. <small>*Please do not block the driveway</small></li>
 </ul>
       </div>{{-- /.modal-body --}}
 
