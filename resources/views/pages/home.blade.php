fdb76244aa71b3319adc143f6068ec051a1d480e
Dom McKellar <dom.mc@outlook.com>
2015-11-09 07:28:40 -0800

    new background photos and updated text

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,13 +7,34 @@
   @include('partials._nav')
 
   {{-- Jumbotron --}}
-  <div id="rsvp" class="jumbotron fullheight"> {{-- .fullheight - adjusts element to takeup entire viewpoint via js --}}
+  <div id="rsvp" class="jumbotron fullheight"
+  style="position: relative;" 
+  > {{-- .fullheight - adjusts element to takeup entire viewpoint via js --}}
     
       <h1 class="title">Nakissa <span class="rwd-line">&amp;</span> Dom</h1>
       <h2>got married!</h2>
 
-      <!-- Trigger the modal with a button -->
-       <div id="drop" class="text-center">
+
+       <div style=" 
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 25%;
+
+       
+       ">
+        <a href="#bigday"><img class="arrow" src="img/arrow.svg" alt="Down arrow" style="
+          width: 60px;
+          height: 60px;
+          padding: 12px;
+          
+          margin-top: 0;
+          "></a>
+          {{--           position: absolute;
+          bottom: 0px; --}}
+      </div> 
+
+{{--        <div id="drop">
         <a href="#bigday" class="text-center"><img class="arrow" src="img/arrow.svg" alt="Down arrow" style="
           width: 60px;
           height: 60px;
@@ -21,14 +42,15 @@
           position: absolute;
           bottom: 5rem;
           display: inline-block;
+          margin
           "></a>
-          {{--           position: absolute;
-          bottom: 0px; --}}
-      </div> 
+
+      </div>  --}}
   </div>{{-- /#rsvp --}}
   {{-- /Jumbotron --}}
 
 </header>
+
 
 
 <div id="top"></div>
@@ -43,6 +65,7 @@
       <div class="col-md-6 col-lg-6">
           <h2 class="featurette-heading">Thank You</h2>
           <p>Mahalo for making the trip out to Kona to help us celebrate our special day :) We know traveling out to the middle the Pacific is no easy feat and we feel so lucky that all of you came out to be there with us.  We hope you had a wonderful time in paradise.  For us, we will always look back on the days we shared with all of you in Kona with such happiness and love.  Thank you so much for being part of the best day (and party) of our lives!</p>
+          <p>Now that we are back in the mainland, we are hoping to gather up as many photos and videos from our wedding week so that we can relive the moments again and again.  If you have any pictures or videos from the ceremony, dinner, dance party, afterparty, Friday pool party, Sunday brunch - or anything else - please click the link below to send these memories with us - we can't wait to see them!</p>
           <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#dropModal">Upload your pics</button>
       </div>
       <div class="col-md-6 col-lg-5 col-lg-offset-1">
@@ -123,7 +146,7 @@
       <div class="col-md-6 col-lg-6">
         <div class="r-pad">
           <h2 class="featurette-heading">Sign our Guestbook</h2>
-          <p>If you have any thoughts, memories, or y-e-s even advice, we’d love to hear from you.</p>
+          <p>If you have any thoughts, memories, or y&nbsp;-&nbsp;e&nbsp;-&nbsp;s even advice, we’d love to hear from you.</p>
          <!-- Trigger the modal with a button -->
           <button type="button" class="btn btn-primary btn-lg b-pad" data-toggle="modal" data-target="#gbModal">Guestbook</button>
         </div>
