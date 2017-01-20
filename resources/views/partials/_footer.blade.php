7258e55082176f7dd586be142248638b111153d5
Dom McKellar <dom.mckellar@gmail.com>
2017-01-20 09:41:10 -0800

    Update footer to include author name and link to twitter account

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="container">
-	<p class="copyright text-center small">Copyright &copy; 2016 <a href="nakissandom.com">nakissandom.com</a> - All Rights Reserved.</p>
+	<p class="copyright text-center small">Copyright &copy; 2016 <a href="https://twitter.com/_dom_mc">Dominic McKellar</a> - All Rights Reserved.</p>
 </footer>
